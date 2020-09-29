d51e24c1602d30d047b79cfb20d65781d41825c3
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-09-29 17:26:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exo_volley/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.exo_volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    SimpleExoPlayer exoPlayer;
+    SimpleExoPlayerView exoPlayerView;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        exoPlayerView = findViewById(R.id.exo);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        jsonParse();
+
+    }
+
+    private void jsonParse() {
+        String url = "15.207.150.183/API/index.php?p=videoTestAPI";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+                            Log.d("manik","working");
+
+                            JSONArray jsonArray = response.getJSONArray("msg");
+
+                                JSONObject video = jsonArray.getJSONObject(0);
+                                String Url = video.getString("mp4Video");
+
+                                Log.d("manik",Url.toString());
+                                loadVideo(Url);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.d("manik",error.toString());
+            }
+        });
+        mQueue.add(request);
+    }
+
+    private void loadVideo(String url) {
+
+        try {
+            BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this,trackSelector);
+            Uri videourl = Uri.parse(url);
+            DefaultHttpDataSourceFactory dataSourceFactory = new DefaultHttpDataSourceFactory("exoplayer_video");
+            ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+            MediaSource mediaSource = new ExtractorMediaSource(videourl,dataSourceFactory,extractorsFactory,null,null);
+            exoPlayerView.setPlayer(exoPlayer);
+            exoPlayer.prepare(mediaSource);
+            exoPlayer.setPlayWhenReady(true);
+        }catch (Exception e){
+            Log.d("manik","exo-player error" + e.toString());
+        }
+
+    }
+}
